eb70f38fb378e9754d9c663b65d5d3e5b9107def
Gabriel Barros <super.gcb18@gmail.com>
2022-02-18 12:17:35 -0300

    Add responsividade e página 404

--- src/components/ListaCards/ListaCards.jsx
+++ src/components/ListaCards/ListaCards.jsx
@@ -17,48 +17,51 @@ function ListaCards({ devsData, deletarDev }) {
 
   return (
     <ul className="listaDevs">
-      {noDevs && "Ainda não há devs cadastrados."}
-      {devsData.map((dev, key) => (
-        <li className="listaDevs__card" key={key}>
-          <img
-            src={dev.avatar}
-            alt={`Foto de ${dev.nome}`}
-            className="card__avatar"
-          />
-          <span className="card__nome">{dev.nome}</span>
-          <span className="card__cargo">{dev.cargo}</span>
-          <div className="card__social">
-            <a
-              href={dev.github}
-              title="GitHub"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={GitHubButton} alt="GitHub" />
-            </a>
-            <a
-              href={dev.linkedin}
-              title="Linkedin"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={LinkedinButton} alt="Linkedin" />
-            </a>
-            <button className="button card__button">Saiba mais</button>
-          </div>
+        {noDevs && "Ainda não há devs cadastrados."}
+        {devsData.map((dev, key) => (
+          <li className="listaDevs__card" key={key}>
+            <img
+              src={dev.avatar}
+              alt={`Foto de ${dev.nome}`}
+              className="card__avatar"
+            />
+            <span className="card__nome">{dev.nome}</span>
+            <span className="card__cargo">{dev.cargo}</span>
+            <div className="card__social">
+              <a
+                href={dev.github}
+                title="GitHub"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={GitHubButton} alt="GitHub" />
+              </a>
+              <a
+                href={dev.linkedin}
+                title="Linkedin"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={LinkedinButton} alt="Linkedin" />
+              </a>
+              <button className="button card__button">Saiba mais</button>
+            </div>
 
-          <div className="card__actions">
-            <button className="action__edit">
-              <MdEdit />
-            </button>
-            <button className="action__delete" onClick={() => {
-              deletarDev(key)
-            }}>
-              <MdDelete />
-            </button>
-          </div>
-        </li>
-      ))}
+            <div className="card__actions">
+              <button className="action__edit">
+                <MdEdit />
+              </button>
+              <button
+                className="action__delete"
+                onClick={() => {
+                  deletarDev(key);
+                }}
+              >
+                <MdDelete />
+              </button>
+            </div>
+          </li>
+        ))}
     </ul>
   );
 }
--- src/pages/NoMatch.jsx
+++ src/pages/NoMatch.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import Lottie from "react-lottie";
+import notfound from "../assets/img/notfound.json";
+import { Link } from 'react-router-dom';
+import '../assets/css/nomatch.css'
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: notfound,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
 
 function NoMatch() {
-  return(
-    <h1>Página não encontrada</h1>
-  )
+  return (
+    <div className="nomatch">
+      <div className="nomatch__ilustracao">
+        <Lottie options={defaultOptions} />
+      </div>
+      <div className="nomatch__action">
+        <h1>Oops... Página não encontrada!</h1>
+        <Link to="/" className="button">Voltar ao início</Link>
+      </div>
+    </div>
+  );
 }
 
 export default NoMatch;
